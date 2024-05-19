2ca6db84829eb7a4f96bbf235c2ad396dd0e22ad
Borhamus <joaquingomez.bv@gmail.com>
2024-05-19 18:45:41 -0300

    mensaje

--- Chaos_Chronicle/logica/admin.py
+++ Chaos_Chronicle/logica/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Carta, Deck, Jugador, Partida, PartidaJugador
 
-# Register your models here.
+admin.site.register(Carta)
+admin.site.register(Deck)
+admin.site.register(Jugador)
+admin.site.register(Partida)
+admin.site.register(PartidaJugador)
