93cb2d86cad0e11551b127fe49ba7a6d0d673e65
Milton Gómez <miltonhernanjosuegomez@gmail.com>
2024-05-19 18:13:04 -0300

    Correción de bug

--- Chaos_Chronicle/logica/models.py
+++ Chaos_Chronicle/logica/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.shortcuts import render
-#from .models import Partida, PartidaJugador
 from django.db import models
 
 class Carta(models.Model):
