6a641ba0a9cc78bcb1e2530ea4c0ee9839acc51b
Milton Gómez <miltonhernanjosuegomez@gmail.com>
2024-06-04 20:45:38 -0300

    Cambios ligeros a los templates

--- Chaos_Chronicle/logica/admin.py
+++ Chaos_Chronicle/logica/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Carta, Jugador, Partida, PartidaJugador
+from .models import Carta, Jugador, Partida, PartidaJugador, Deck
 
 admin.site.register(Carta)
 admin.site.register(Jugador)
 admin.site.register(Partida)
 admin.site.register(PartidaJugador)
+admin.site.register(Deck)
